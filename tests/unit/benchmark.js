4f2ef2861e6b853fff441832d758cd80b13250af
Jason Cheatham <j.cheatham@gmail.com>
2015-09-25 08:25:24 -0400

    Reorganize around Test
    Instead of wrapping at the suite level, wrap at the test level, and use
    the standard Intern execution engine to run the tests.

--- tests/unit/benchmark.js
+++ tests/unit/benchmark.js
@@ -1,15 +1,34 @@
 define([
     'intern!benchmark'
-], function (registerBenchmarkSuite) {
-    registerBenchmarkSuite({
+], function (registerSuite) {
+    registerSuite({
         name: 'example benchmarks',
-        'test1': function () {
+
+        test1: function () {
             return 2 * 2;
         },
-        'test2': function () {
-            [ 1, 2, 3, 4, 5 ].forEach(function (item) {
-                item = item * item;
-            });
-        }
+
+        test2: (function () {
+			function test() {
+				[ 1, 2, 3, 4, 5 ].forEach(function (item) {
+					item = item * item;
+				});
+			}
+
+			test.options = {
+			};
+
+			return test;
+        })(),
+
+		nested: {
+			nested1: function () {
+				return 23 * 23;
+			},
+
+			nested2: function () {
+				return 23 / 12;
+			}
+		}
     });
 });
